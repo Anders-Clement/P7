d269481a9923f382e304a555eb2cf96abb94bdaa
PepiHax <82349196+PepiHax@users.noreply.github.com>
2021-10-12 18:38:30 +0200

    Njit for faster execution time

--- ProjectHOG/calc_hog.py
+++ ProjectHOG/calc_hog.py
@@ -1,32 +1,22 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
+from numba import njit
 
 kernel = np.array([[-1],
                    [0],
                    [1]])
 
 kernel2 = np.array([[-1, 0, 1]])
+window_size = 8
 
-
-def calculate_Hog(frame):
-    channels = cv.split(frame)
-
-    mags = []
-    angles = []
-    for channel in channels:
-        gy = (np.float32(cv.filter2D(channel, -1, kernel)) / 255.0)
-        gx = (np.float32(cv.filter2D(channel, -1, kernel2)) / 255.0)
-        mag, angle = cv.cartToPolar(gx, gy, angleInDegrees=True)
-        mags.append(mag)
-        angles.append(angle)
-
-    window_size = 8
+@njit()
+def cal_histogram(angles, mags):
     SizeX = int((angles[0].shape[0]) / window_size)
     SizeY = int((angles[0].shape[1]) / window_size)
 
-    histogram_list = np.array([[[0.0 for _ in range(9)] for _ in range(SizeX)] for _ in range(SizeY)])
-
+    #histogram_list = np.array([[[0.0 for _ in range(9)] for _ in range(SizeX)] for _ in range(SizeY)])
+    histogram_list = np.zeros((SizeY, SizeX, 9))
     for x in range(0, SizeX):
         for y in range(0, SizeY):
             r = x * window_size
@@ -40,9 +30,9 @@ def calculate_Hog(frame):
                     working_mag = 0.0
                     for k in range(len(angles)):
                         # This is for bins spaced over 0◦–180◦, i.e. the ‘sign’ of the gradient is ignored.
-                        if abs(mags[k][r + i][c + j]) > working_mag:
-                            working_angle = abs(angles[k][r + i][c + j])
-                            working_mag = abs(mags[k][r + i][c + j])
+                        if mags[k][r + i][c + j] > working_mag:
+                            working_angle = angles[k][r + i][c + j]
+                            working_mag = mags[k][r + i][c + j]
 
                     if working_angle > 180:
                         working_angle -= 180
@@ -58,6 +48,23 @@ def calculate_Hog(frame):
                     else:
                         histogram_list[y, x][bin] += working_mag * per_for_next_bottom
                         histogram_list[y, x][bin + 1] += working_mag * per_for_next_upper
+
+    return histogram_list
+
+
+def calculate_Hog(frame, draw_arrows = False):
+    channels = cv.split(frame)
+
+    mags = []
+    angles = []
+    for channel in channels:
+        gy = (np.float32(cv.filter2D(channel, -1, kernel)) / 255.0)
+        gx = (np.float32(cv.filter2D(channel, -1, kernel2)) / 255.0)
+        mag, angle = cv.cartToPolar(gx, gy, angleInDegrees=True)
+        mags.append(mag)
+        angles.append(angle)
+
+    histogram_list = cal_histogram(angles, mags)
 
     # print(histogram_list.shape)
     # fig, axs = plt.subplots(histogram_list.shape[1], histogram_list.shape[0])
@@ -76,27 +83,28 @@ def calculate_Hog(frame):
                     new_hist = np.append(new_hist, np.array(histogram_list[x + x_new, y + y_new]))
             # L2-Hys normalize then clip then normalize.
             divide_value = np.sqrt(np.linalg.norm(new_hist) ** 2 + e)
-            new_table = [max(min(x, 0.2), 0) for x in (new_hist / divide_value)]
+            new_table = [min(x, 0.2) for x in (new_hist / divide_value)]
             divide_value2 = np.sqrt(np.linalg.norm(new_table) ** 2 + e)
             row_x.append(new_table / divide_value2)
         feature_vector.append(row_x)
 
     print(f'Number of HOG features = {len(feature_vector) * len(feature_vector[0]) * len(feature_vector[0][0])}')
 
-    for x in range(0, histogram_list.shape[0]):
-        for y in range(0, histogram_list.shape[1]):
-            center_x = x * window_size + int(window_size / 2)
-            center_y = y * window_size + int(window_size / 2)
-            for i, value in enumerate(histogram_list[x, y]):
-                end_x = int(center_x + value * np.sin((i * 20) * np.pi / 180))
-                end_y = int(center_y + value * np.cos((i * 20) * np.pi / 180))
-                cv.arrowedLine(frame, (center_x, center_y), (end_x, end_y), (0, 0, 255))
+    if (draw_arrows):
+        for x in range(0, histogram_list.shape[0]):
+            for y in range(0, histogram_list.shape[1]):
+                center_x = x * window_size + int(window_size / 2)
+                center_y = y * window_size + int(window_size / 2)
+                for i, value in enumerate(histogram_list[x, y]):
+                    end_x = int(center_x + value * np.sin((i * 20) * np.pi / 180) * 20)
+                    end_y = int(center_y + value * np.cos((i * 20) * np.pi / 180) * 20)
+                    cv.arrowedLine(frame, (center_x, center_y), (end_x, end_y), (0, 0, 255))
 
     return feature_vector, frame
 
 
 if __name__ == "__main__":
-    cap = cv.VideoCapture(0)
+    cap = cv.VideoCapture(r"D:\SyncFolder\P7\P7Group\ProjectHOG\Test.mp4")
     # Check if the webcam is opened correctly
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
@@ -104,7 +112,7 @@ if __name__ == "__main__":
     while True:
         ret, frame = cap.read()
 
-        histrograms, frame = calculate_Hog(frame)
+        histrograms, frame = calculate_Hog(frame, True)
 
         plt.figure()
         plt.imshow(cv.cvtColor(frame, cv.COLOR_BGRA2RGB))
