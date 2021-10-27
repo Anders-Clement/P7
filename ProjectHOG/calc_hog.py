d69c9d756b93ec40a58959246995d73839b04816
Anders Clement <andersclement96@gmail.com>
2021-10-27 10:19:52 +0200

    added openCV HoG for comparison

--- ProjectHOG/calc_hog.py
+++ ProjectHOG/calc_hog.py
@@ -52,6 +52,28 @@ def cal_histogram(angles, mags):
     return histogram_list
 
 
+def calculate_Hog_OPENCV(frame, notUsed=None):
+    winSize = (64,128)
+    blockSize = (16,16)
+    blockStride = (8,8)
+    cellSize = (8,8)
+    nbins = 9
+    derivAperture = 1
+    winSigma = 4.
+    histogramNormType = 0
+    L2HysThreshold = 2.0000000000000001e-01
+    gammaCorrection = 0
+    nlevels = 64
+    hog = cv.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
+                            histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+    #compute(img[, winStride[, padding[, locations]]]) -> descriptors
+    winStride = (8,8)
+    padding = (8,8)
+    locations = ((0,0),)
+    hist = hog.compute(frame,winStride,padding,locations)
+    return hist.ravel(), None
+
+
 def calculate_Hog(frame, draw_arrows = False):
     channels = cv.split(frame)
 
@@ -104,7 +126,7 @@ def calculate_Hog(frame, draw_arrows = False):
 
 
 if __name__ == "__main__":
-    cap = cv.VideoCapture(r"D:\SyncFolder\P7\P7Group\ProjectHOG\Test.mp4")
+    cap = cv.VideoCapture(r"Test.mp4")
     # Check if the webcam is opened correctly
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
