c41b754e5195f58852dcae416acee8f892746f59
nuc <nuc@whatever.com>
2021-11-22 14:17:09 +0100

    StuffHansMade

--- catkin_ws/src/tracking/scripts/data_ass.py
+++ catkin_ws/src/tracking/scripts/data_ass.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 from logging import fatal
+from math import atan2
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
@@ -14,6 +15,7 @@ from geometry_msgs.msg import QuaternionStamped
 from geometry_msgs.msg import PointStamped
 
 from transtonumpy import msg_to_se3
+import copy
 
 class ourFilter():
 	def __init__(self):
@@ -29,7 +31,8 @@ class ourFilter():
 		laser_point.point.x = msg.quaternion.x
 		laser_point.point.y = msg.quaternion.y
 		laser_point.point.z = msg.quaternion.z
-		newPoint = laser_point
+		newPoint = copy.deepcopy(laser_point)
+		newPoint2 = copy.deepcopy(laser_point)
 		time_now = msg.header.stamp
 
 		if self.start_time == None or time_now.to_sec() < self.start_time:
@@ -38,16 +41,25 @@ class ourFilter():
 		self.last_time = time.time()
 		trans2 = None
 		targetFrame = "odom"
+		angleFrame = "base_link"
 		try:
-			trans = self.tfBuffer.lookup_transform(targetFrame, msg.header.frame_id, time_now, rospy.Duration(1))
+			trans = self.tfBuffer.lookup_transform(targetFrame, laser_point.header.frame_id, time_now, rospy.Duration(1))
 			trans = msg_to_se3(trans)
 			newPoint_stuff = np.matmul(trans, np.array([laser_point.point.x, laser_point.point.y, laser_point.point.z, 1]).T )
 			newPoint.point.x = newPoint_stuff[0]
 			newPoint.point.y = newPoint_stuff[1]
 			newPoint.point.z = newPoint_stuff[2]
 
-			trans2 = self.tfBuffer.lookup_transform(targetFrame, "flat_body", time_now, rospy.Duration(1))
-			next_tuple = (time_now.to_sec(), newPoint.point.x, newPoint.point.y, newPoint.point.z, trans2.transform.translation.x, trans2.transform.translation.y, trans2.transform.translation.z)
+			trans2 = self.tfBuffer.lookup_transform(angleFrame, laser_point.header.frame_id, time_now, rospy.Duration(1))
+			trans2 = msg_to_se3(trans2)
+			newPoint_stuff = np.matmul(trans2, np.array([laser_point.point.x, laser_point.point.y, laser_point.point.z, 1]).T )
+			newPoint2.point.x = newPoint_stuff[0]
+			newPoint2.point.y = newPoint_stuff[1]
+			newPoint2.point.z = newPoint_stuff[2]
+			
+			trans3 = self.tfBuffer.lookup_transform(targetFrame, "flat_body", time_now, rospy.Duration(1))
+			next_tuple = (time_now.to_sec(), newPoint.point.x, newPoint.point.y, newPoint.point.z, 
+											trans3.transform.translation.x, trans3.transform.translation.y, trans3.transform.translation.z, atan2(newPoint2.point.y, newPoint2.point.x))
 
 			self.data.append(next_tuple)
 		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
@@ -59,7 +71,7 @@ class ourFilter():
 		self.listener = tf2_ros.TransformListener(self.tfBuffer)
 		#new_rate = rospy.Rate(10)
 		while not rospy.is_shutdown():
-			if (time.time() - self.last_time) > 5:
+			if (time.time() - self.last_time) > 50:
 				self.data = sorted(self.data, key=lambda a: a[0])
 
 				if len(self.data) == 0:
@@ -67,7 +79,7 @@ class ourFilter():
 
 				first_time = self.data[0][0]
 				for x in range(len(self.data)):
-					self.data[x] = (self.data[x][0] - first_time, self.data[x][1], self.data[x][2], self.data[x][3], self.data[x][4], self.data[x][5], self.data[x][6]) 
+					self.data[x] = (self.data[x][0] - first_time, self.data[x][1], self.data[x][2], self.data[x][3], self.data[x][4], self.data[x][5], self.data[x][6], self.data[x][7]) 
 
 				np_data = np.array(self.data)
 				print(np_data)
