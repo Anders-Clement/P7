167fb7ff1bd6e16f9943e387a2d8b39ba9cb27a2
nuc <nuc@whatever.com>
2021-12-03 10:54:01 +0100

    AndersStuff

--- catkin_ws/src/tracking/scripts/kalman_filter_node.py
+++ catkin_ws/src/tracking/scripts/kalman_filter_node.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3.6 
+
 import rospy
 import tf2_ros
 
@@ -20,69 +22,86 @@ class kalmanFilterNode:
         self.detection_sub = rospy.Subscriber('/people_detections', QuaternionStamped, self.detection_callback, queue_size=10)
         self.marker_pub = rospy.Publisher('kalman_filter_visualization_markers', Marker, queue_size=10)
         self.kalmanFilter = None
-        self.lastGoodDetectionTime = rospy.Time.now()
+        self.lastGoodDetectionTime = rospy.Time.now().to_sec()
         self.TIMEOUTLIMIT = 10
         
 
     def detection_callback(self, msg):
-        # initialize kalman filter if not done yet, of if KF is lost
-        if self.kalmanFilter is None or rospy.Time.now() - self.lastGoodDetectionTime > self.TIMEOUTLIMIT:
-            # get position of spot, to initialize kalman filter
-            try:
-                self.trans = self.tfBuffer.lookup_transform('base_link', 'odom', rospy.Time.now())
-            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-                return
-            transformation_matrix = msg_to_se3(self.trans)
-            # initialize the filter 2 meter behind the robot
-            startPos = np.array([-2,0,0,1])
-            transformedPos = np.matmul(transformation_matrix, startPos)
-            self.kalmanFilter = kalmanFilter(rospy.Time.now())
-            self.kalmanFilter.x = np.array([transformedPos[0],transformedPos[1],0,0,0,0])
+        try:
+            self.trans = self.tfBuffer.lookup_transform('base_link', 'odom', msg.header.stamp)
+            transformation_matrix_odom_body = msg_to_se3(self.trans)
+            transformation_matrix_body_odom = np.linalg.inv(transformation_matrix_odom_body)
+            # initialize kalman filter if not done yet, of if KF is lost
+            if self.kalmanFilter is None or rospy.Time.now().to_sec() - self.lastGoodDetectionTime > self.TIMEOUTLIMIT:
+                # initialize the filter 2 meter behind the robot
+                startPos = np.array([-2,0,0,1])
+                transformedPos = np.matmul(transformation_matrix_body_odom, startPos)
+                self.kalmanFilter = kalmanFilter(rospy.Time.now().to_sec())
+                self.kalmanFilter.x = np.array([transformedPos[0],transformedPos[1],0,0,0,0])
+                print('init KF, x_y: ', self.kalmanFilter.x[:2])         
 
-        if self.kalmanFilter.predict(rospy.Time.now(), np.array([msg.quaternion.x, msg.quaternion.y, 0]) ):
-            # find distance to robot and publish it
-            dist = np.linalg.norm(np.array(self.trans.translation)[:2] - self.kalmanFilter.x[:2])
-            distance_to_robot_msg = Float32()
-            distance_to_robot_msg.data = dist
-            self.distance_to_robot_pub.publish(distance_to_robot_msg)
-            self.pub_KF_x_pos()
+            detectionPos = np.matmul(transformation_matrix_body_odom, [msg.quaternion.x, msg.quaternion.y, 0, 1])
+            detectionPos[2] = 0  
+            detectionPos = detectionPos[:3] 
+            if self.kalmanFilter.predict(rospy.Time.now().to_sec(), np.array(detectionPos) ):
+                self.lastGoodDetectionTime = rospy.Time.now().to_sec()
+                # find distance to robot and publish it
+                # SPOTPOS IS WRONG
+                spotPos = np.array([self.trans.transform.translation.x, self.trans.transform.translation.y])
+                dist = np.linalg.norm(np.array(spotPos - self.kalmanFilter.x[:2]))
+                distance_to_robot_msg = Float32()
+                distance_to_robot_msg.data = dist
+                self.distance_to_robot_pub.publish(distance_to_robot_msg)
+                print('good detection, KF.x: ', self.kalmanFilter.x, detectionPos, spotPos, dist)
 
-        self.pub_KF_pred_x_pos()
+
+            self.pub_KF_x_pos() 
+            self.pub_KF_pred_x_pos()
+            
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            print('failed transform kalman filter')
+            return
+ 
     
 
     def get_marker(self, color, xyPos, id):
-        self.robotMarker = Marker()
-        self.robotMarker.header.frame_id = 'odom'
-        self.robotMarker.header.stamp    = rospy.get_rostime()
-        self.robotMarker.ns = "kalman_filter"
-        self.robotMarker.type = 2 # sphere
-        self.robotMarker.action = 0
-        self.robotMarker.pose.position.x = xyPos[0]
-        self.robotMarker.pose.position.y = xyPos[1]
-        self.robotMarker.pose.position.z = self.trans.translation.z
-        self.robotMarker.pose.orientation.x = 0
-        self.robotMarker.pose.orientation.y = 0
-        self.robotMarker.pose.orientation.z = 0
-        self.robotMarker.pose.orientation.w = 1.0
-        self.robotMarker.scale.x = 0.1
-        self.robotMarker.scale.y = 0.1
-        self.robotMarker.scale.z = 0.1
-        self.robotMarker.color.r = color[0]
-        self.robotMarker.color.g = color[1]
-        self.robotMarker.color.b = color[2]
-        self.robotMarker.color.a = 1.0
-        self.robotMarker.id = id
-        self.robotMarker.lifetime = rospy.Duration(self.TIMEOUTLIMIT)
+        marker = Marker()
+        marker.header.frame_id = 'odom'
+        marker.header.stamp    = rospy.get_rostime()
+        marker.ns = "kalman_filter"
+        marker.type = 2 # sphere
+        marker.action = 0
+        marker.pose.position.x = xyPos[0]
+        marker.pose.position.y = xyPos[1]
+        marker.pose.position.z = self.trans.transform.translation.z
+        marker.pose.orientation.x = 0
+        marker.pose.orientation.y = 0
+        marker.pose.orientation.z = 0
+        marker.pose.orientation.w = 1.0
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.r = color[0]
+        marker.color.g = color[1]
+        marker.color.b = color[2]
+        marker.color.a = 1.0
+        marker.id = id
+        marker.lifetime = rospy.Duration(self.TIMEOUTLIMIT)
+        return marker
 
 
     def pub_KF_x_pos(self):
-        marker = self.get_marker([255,0,0], self.kalmanFilter.x[:2],0)
-        self.marker_pub.publish(marker)
+        if self.kalmanFilter is not None:
+            x_pos = self.kalmanFilter.x[:2]
+            marker = self.get_marker([255,0,0], x_pos,0)
+            self.marker_pub.publish(marker)
 
 
     def pub_KF_pred_x_pos(self):
-        marker = self.get_marker([255,0,0], self.kalmanFilter.x_pred[:2],0)
-        self.marker_pub.publish(marker)    
+        if self.kalmanFilter is not None:
+            pred_pos = self.kalmanFilter.x_pred[:2]
+            marker = self.get_marker([0,255,0], pred_pos,1)
+            self.marker_pub.publish(marker)    
 
 
 if __name__ == '__main__':
